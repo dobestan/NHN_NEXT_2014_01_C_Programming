488be4a80465fcb98c486fe66cf776beee170fb4
dobestan <dobestan@gmail.com>
2014-04-16 08:13:28 +0900

    add recursive algorithm

--- 20140404/homework/hw03_4.c
+++ 20140404/homework/hw03_4.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-#define bread 500
-#define snack 700
-#define drink 400
+#define BREAD 500
+#define SNACK 700
+#define DRINK 400
 
 int get_current_money(void);
+
 void print_options(int);
+void print_options_recursive(int, int, int, int);
 
 int main() {
-  print_options(get_current_money());
+  print_options_recursive(get_current_money()-(BREAD + SNACK + DRINK), 1, 1, 1);
   return 0;
 }
 
@@ -20,11 +22,30 @@ int get_current_money(void) {
 }
 
 void print_options(int money) {
-  for ( int i = 1 ; i <= money / bread ; i++ ) {
-    for ( int j = 1 ; j <= (money - i * bread) / snack ; j++) {
-      for ( int k = 1 ; k <= (money - i * bread - j * snack) / drink ; k++ ) {
-        if ( money == i*bread + j*snack + k*drink ) printf("BREAD(%d), SNACK(%d), DRINK(%d)\n", i , j, k );
+  for ( int i = 1 ; i <= money / BREAD ; i++ ) {
+    for ( int j = 1 ; j <= (money - i * BREAD) / SNACK ; j++) {
+      for ( int k = 1 ; k <= (money - i * BREAD - j * SNACK) / DRINK ; k++ ) {
+        if ( money == i*BREAD + j*SNACK + k*DRINK ) printf("BREAD(%d), SNACK(%d), DRINK(%d)\n", i , j, k );
       }
     }
   }
 }
+
+void print_options_recursive(int money, int bread, int snack, int drink) {
+  // Usage : print_options_recursive(get_current_money()-(BREAD + SNACK + DRINK), 1, 1, 1);
+
+  if ( money < 0 ) return;
+  if ( money == 0 ) printf("BREAD(%d), SNACK(%d), DRINK(%d)\n", bread , snack, drink );
+
+  print_options_recursive(money - BREAD, bread+1, snack, drink);
+  print_options_recursive(money - SNACK, bread, snack+1, drink);
+  print_options_recursive(money - DRINK, bread, snack, drink+1);
+}
+
+
+
+
+
+
+
+
