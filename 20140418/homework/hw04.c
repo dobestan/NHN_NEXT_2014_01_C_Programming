3460fc35c3b39a88f89926a40187b382a203033b
dobestan <dobestan@gmail.com>
2014-04-28 19:58:20 +0900

    add hw04.c

--- 20140418/homework/hw04.c
+++ 20140418/homework/hw04.c
@@ -23,8 +23,11 @@ char * make_words_inverse(char *);
 void make_words_inverse_test(char *, char *);
 void make_words_inverse_tests(void);
 
+void make_words_inverse_exec(void);
+
 int main() {
-  // make_words_inverse_tests();
+  make_words_inverse_tests();
+  make_words_inverse_exec();
   return 0;
 }
 
@@ -40,6 +43,36 @@ char * make_words_inverse(char * src) {
   // char * foo = "foo";
   // return foo;
   // source for TDD ( RED )
+
+  /// 일단 핵심적인 아이디어는 Stack을 이용하는 것
+  /// 그냥 배열을 Stack으로 이용하자.
+  ///
+  /// 공백이 나올 때 까지 진행한다
+  /// 공백이 나오면 지금까지 저장했던 문자열을 출력한다.
+
+  char * dest = (char *) malloc (sizeof(char) * ( strlen(src) + 1) );
+  /// 마지막에 NULL을 넣기 위해서 strlen(src)보다는 하나 더 크게 만들어주자.
+  int dest_index = 0;
+
+  char store_word[10] = {'\0'};
+  int store_word_index = 0;
+  /// for stack
+
+  while ( *src ) { // 일단은 \0이 나올때까지 진행한다.
+    store_word[store_word_index++] = *src++;
+
+    if ( *src == ' ' || *src == '\0' ) {
+      if ( *src == ' ') src++;
+      for ( int i = store_word_index-1 ; i >= 0 ; i-- ) {
+        dest[dest_index++] = store_word[i];
+      }
+      store_word_index = 0;
+      dest[dest_index++] = ' ';
+    }
+  }
+
+  dest[dest_index-1] = '\0';
+  return dest;
 }
 
 void make_words_inverse_test(char * src, char * expected) {
@@ -56,3 +89,26 @@ void make_words_inverse_tests(void) {
   make_words_inverse_test("I am a student", "I ma a tneduts");
   printf("----------\n");
 }
+
+void make_words_inverse_exec(void) {
+  char foo[100] = {'\0'}; // init
+  char * bar;
+  printf("Enter a Sentence : ");
+  gets(foo);
+  bar = make_words_inverse(foo);
+
+  printf("[%s] -> [%s]\n", foo, bar);
+}
+
+/*
+ * --- result ---
+ *  ---test---
+ *  [SUCCESS] input is [apple], output should be [elppa]
+ *  [SUCCESS] input is [hello world], output should be [olleh dlrow]
+ *  [SUCCESS] input is [I am a student], output should be [I ma a tneduts]
+ *  ----------
+ *  warning: this program uses gets(), which is unsafe.
+ *  Enter a Sentence : my name is dobestan
+ *  [my name is dobestan] -> [ym eman si natsebod]
+ * --------------
+ */
